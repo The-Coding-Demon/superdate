16e1bce68683493a87339a851032e6376ba75ecf
The_Coding_Demon <ssmithmedia2015@gmail.com>
2023-06-19 20:06:21 +0100

    change in logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <nav class="navbar fixed-bottom text-center footer">
-      <a class="navbar-brand text-center ">©SuperDate</a>
+      <a class="navbar-brand text-center">©SuperDate</a>
     </nav>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import { HomeButton } from "../components/HomeButton";
-import { QuestionsBox } from "../components/QuestionsBox";
 import { BackingLayerBoxes } from "../components/BackingLayerBoxes";
 export const Home = () => {
   return (
