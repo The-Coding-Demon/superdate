1953b05c5474e3caf8a4e5890baeb0a52c88c66e
surajverma2587 <surajverma2587@gmail.com>
2023-07-05 19:56:55 +0100

    add basic map

--- src/pages/MapDirections.js
+++ src/pages/MapDirections.js
@@ -1,8 +1,23 @@
+import Map, { Marker } from "react-map-gl";
 import { MapDetails } from "../components/MapDetails";
+import "mapbox-gl/dist/mapbox-gl.css";
+
 export const MapDirections = ({ stateProps }) => {
   return (
     <div>
-      <MapDetails stateProps={stateProps} />
+      {/* <MapDetails stateProps={stateProps} /> */}
+      <Map
+        initialViewState={{
+          latitude: 37.8,
+          longitude: -122.4,
+          zoom: 14,
+        }}
+        style={{ width: 800, height: 600 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+      >
+        <Marker longitude={-122.4} latitude={37.8} color="red" />
+      </Map>
     </div>
   );
 };
